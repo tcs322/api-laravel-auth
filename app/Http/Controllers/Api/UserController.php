7589ce728a494716b5dbce47eadecb00d03e954e
Thyerre <thyerre.sampaio@kolaresti.com.br>
2024-02-27 18:50:19 -0300

    classe UserResource

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index() {
-        return User::all();
+        $users = User::paginate();
+
+        return UserResource::collection($users);
     }
 }
